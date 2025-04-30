41dffd9921354e1db02c909b6be70420e63b1045
Maximilian Wolf <maxi645@outlook.de>
2025-04-30 14:17:43 +0200

    Added new files for 2. Lab

---
+++ components/register/register_file.vhdl
@@ -0,0 +1,19 @@
+-- Laboratory RA solutions/versuch2
+-- Sommersemester 25
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.math_real.all;
+  use work.constant_package.all;
+  use work.types_package.all;
+
+entity register_file is
+end entity register_file;
+
+architecture behavior of register_file is
+end architecture behavior;
