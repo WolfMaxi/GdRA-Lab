98e251ec226199555973e205a49f7b7f44a59909
Esad Cekmeci <cekmeci.esad@gmx.de>
2025-05-04 15:04:26 +0200

    Bash nochmal verändert

--- scripts/registerfiles/register_file_tb.sh
+++ scripts/registerfiles/register_file_tb.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 workdir="../../"
-rm "${workdir}/vhdl/"*.cf
+rm "${workdir}vhdl/"*.cf
 
-echo "${workdir}vhdl"
-
-ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}/packages/constant_package.vhdl"
-ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}/packages/type_packages.vhdl"
-ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}/components/registerfile/register_file.vhdl"
-ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}/testbenches/registerfile/register_file_tb.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/constant_package.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/type_packages.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/registerfile/register_file.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}testbenches/registerfile/register_file_tb.vhdl"
 ghdl -e --std=08 --workdir="${workdir}vhdl" register_file_tb
-ghdl -r --std=08 --workdir="${workdir}vhdl" register_file_tb --vcd="${workdir}/vhdl/vcd/register/register_file_tb.vcd" --stop-time=120ns
+ghdl -r --std=08 --workdir="${workdir}vhdl" register_file_tb --vcd="${workdir}vhdl/vcd/register_file_tb.vcd" --stop-time=120ns
 #gtkwave ../../vhdl/vcd/register/register_file_tb.vcd
