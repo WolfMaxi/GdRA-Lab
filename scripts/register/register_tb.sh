82f01f2294da3fca7876ca3cffc4d9ff0f901556
Maximilian Wolf <maxi645@outlook.de>
2025-05-06 15:11:44 +0200

    Bug fixes for merge

--- scripts/register/register_tb.sh
+++ scripts/register/register_tb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 workdir="../../"
-rm "${workdir}/vhdl/"*.cf
-ghdl -a --std=08 --workdir="${workdir}/vhdl" "${workdir}/packages/constant_package.vhdl"
-ghdl -a --std=08 --workdir="${workdir}/vhdl" "${workdir}/components/register/PipelineRegister.vhdl"
-ghdl -a --std=08 --workdir="${workdir}/vhdl" "${workdir}/testbenches/register/PipelineRegister_tb.vhdl"
-ghdl -e --std=08 --workdir="${workdir}/vhdl" PipelineRegister_tb
-ghdl -r --std=08 --workdir="${workdir}/vhdl" PipelineRegister_tb --vcd="${workdir}/vhdl/vcd/register/register_tb.vcd" --stop-time=100ns
+rm "${workdir}vhdl/"*.cf
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/constant_package.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/register/PipelineRegister.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}testbenches/register/PipelineRegister_tb.vhdl"
+ghdl -e --std=08 --workdir="${workdir}vhdl" PipelineRegister_tb
+ghdl -r --std=08 --workdir="${workdir}vhdl" PipelineRegister_tb --vcd="${workdir}vhdl/vcd/register_tb.vcd" --stop-time=100ns
