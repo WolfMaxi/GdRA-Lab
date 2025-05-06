26cc669e6fef6a70d41eb0213314fc0c4517ee2f
Maximilian Wolf <maxi645@outlook.de>
2025-05-06 15:43:07 +0200

    Added new files for 3.lab

---
+++ components/decoder/decoder.vhdl
@@ -0,0 +1,21 @@
+-- Laboratory RA solutions/versuch3
+-- Sommersemester 25
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.Constant_package.all;
+use work.types.all;
+
+entity decoder is
+    -- begin solution:
+    -- end solution!!
+end entity decoder;
+architecture arc of decoder is
+    -- begin solution:
+    -- end solution!!
+end architecture;
---
+++ components/signExtender/signExtension.vhdl
@@ -0,0 +1,27 @@
+-- Laboratory RA solutions/versuch3
+-- Sommersemester 25
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
+-- ========================================================================
+-- Description:  Sign extender for a RV32I processor. Takes the entire instruction
+--               and produces a 32-Bit value by sign-extending, shifting and piecing
+--               together the immedate value in the instruction.
+-- ========================================================================
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use work.constant_package.all;
+
+entity signExtension is
+-- begin solution:
+   -- end solution!!
+end entity signExtension;
+
+architecture arc of signExtension is
+-- begin solution:
+ -- end solution!!
+end architecture arc;
