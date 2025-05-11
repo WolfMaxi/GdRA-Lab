6792311b57415f6c4399ffc96dd0c4c273c4d3f3
Maximilian Wolf <maxi645@outlook.de>
2025-05-11 15:34:22 +0200

    Added script for easier zipping

--- scripts/clear.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-workdir="../"
-rm "${workdir}vhdl/"*.cf
-rm "${workdir}vhdl/vcd/"*.vcd
---
+++ zip.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+suffix="V3"
+zip -r "lab.zip" . -x ".gitignore" ".git/*" "vhdl_ls.toml" "documents/*" "zip.sh" "*.cf" "*.vcd"
+cp "lab.zip" "Maximilian_Wolf_${suffix}.zip"
+mv "lab.zip" "Esad_Cekmeci_${suffix}.zip"
