97a59cdd43fcde8df2cb1f73e48c5b0c5f9e32c0
Maximilian Wolf <maxi645@outlook.de>
2025-05-11 14:57:45 +0200

    File Formatting for merge

--- components/signExtender/signExtension.vhdl
+++ components/signExtender/signExtension.vhdl
@@ -12,55 +12,55 @@
 -- ========================================================================
 
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-  use work.constant_package.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.constant_package.all;
 
 entity signExtension is
--- begin solution:
-    generic(
+    -- begin solution:
+    generic (
         word_width : integer := WORD_WIDTH
     );
     port (
-        pi_instr        : in std_logic_vector(word_width - 1 downto 0);
-        po_jumpImm      : out std_logic_vector(word_width - 1 downto 0);
-        po_branchImm    : out std_logic_vector(word_width - 1 downto 0);
-        po_unsignedImm  : out std_logic_vector(word_width - 1 downto 0);
+        pi_instr : in std_logic_vector(word_width - 1 downto 0);
+        po_jumpImm : out std_logic_vector(word_width - 1 downto 0);
+        po_branchImm : out std_logic_vector(word_width - 1 downto 0);
+        po_unsignedImm : out std_logic_vector(word_width - 1 downto 0);
         po_immediateImm : out std_logic_vector(word_width - 1 downto 0);
-        po_storeImm     : out std_logic_vector(word_width - 1 downto 0) 
+        po_storeImm : out std_logic_vector(word_width - 1 downto 0)
     );
-   -- end solution!!
+    -- end solution!!
 end entity signExtension;
 
 architecture arc of signExtension is
--- begin solution:
+    -- begin solution:
     --signal s_jumpImm : std_logic_vector(20 downto 0) := (others => '0');
     --signal s_branchImm : std_logic_vector(12 downto 0) := (others => '0');
     --signal s_immediateImm : std_logic_vector(11 downto 0) := (others => '0');
     --signal s_storeImm : std_logic_vector(11 downto 0) := (others => '0');
 
+begin
+    process (pi_instr)
+
+        variable v_immediateImm : signed (11 downto 0) := (others => '0');
+        variable v_storeImm : signed (11 downto 0) := (others => '0');
+        variable v_branchImm : signed (12 downto 0) := (others => '0');
+        variable v_jumpImm : signed (20 downto 0) := (others => '0');
+
     begin
-        process(pi_instr)
+        v_immediateImm := signed(pi_instr(31 downto 20));
+        po_immediateImm <= std_logic_vector(resize(v_immediateImm, word_width));
 
-            variable v_immediateImm : signed (11 downto 0) := (others => '0');
-            variable v_storeImm : signed (11 downto 0) := (others => '0');
-            variable v_branchImm : signed (12 downto 0) := (others => '0');
-            variable v_jumpImm : signed (20 downto 0) := (others => '0');
+        v_storeImm := signed(pi_instr(31 downto 25) & pi_instr(11 downto 7));
+        po_storeImm <= std_logic_vector(resize(v_storeImm, word_width));
 
-        begin
-            v_immediateImm := signed(pi_instr(31 downto 20));
-            po_immediateImm <= std_logic_vector(resize(v_immediateImm, word_width));
+        v_branchImm := signed(pi_instr(31) & pi_instr(7) & pi_instr(30 downto 25) & pi_instr(11 downto 8) & '0');
+        po_branchImm <= std_logic_vector(resize(v_branchImm, word_width));
 
-            v_storeImm := signed(pi_instr(31 downto 25) & pi_instr(11 downto 7));
-            po_storeImm     <= std_logic_vector(resize(v_storeImm, word_width));
+        v_jumpImm := signed(pi_instr(31) & pi_instr(19 downto 12) & pi_instr(20) & pi_instr(30 downto 21) & '0');
+        po_jumpImm <= std_logic_vector(resize(v_jumpImm, word_width));
 
-            v_branchImm := signed(pi_instr(31) & pi_instr(7) & pi_instr(30 downto 25) & pi_instr(11 downto 8) & '0');
-            po_branchImm    <= std_logic_vector(resize(v_branchImm, word_width));
-
-            v_jumpImm := signed(pi_instr(31) & pi_instr(19 downto 12) & pi_instr(20) & pi_instr(30 downto 21) & '0');
-            po_jumpImm      <= std_logic_vector(resize(v_jumpImm, word_width));
-
-            po_unsignedImm <= pi_instr(word_width - 1 downto 12) & (11 downto 0 => '0');
-        end process;
--- end solution!!
+        po_unsignedImm <= pi_instr(word_width - 1 downto 12) & (11 downto 0 => '0');
+    end process;
+    -- end solution!!
 end architecture arc;
