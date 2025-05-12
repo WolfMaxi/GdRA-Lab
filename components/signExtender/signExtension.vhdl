bbaa3563a7cbfcb91522886c143f944a300a80cb
Esad Cekmeci <cekmeci.esad@gmx.de>
2025-05-12 22:28:17 +0200

    Added Comments to SignExtension.vhdl for mor Comfortable understandment

--- components/signExtender/signExtension.vhdl
+++ components/signExtender/signExtension.vhdl
@@ -16,16 +16,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.constant_package.all;
 
-entity signExtension is
+entity signExtension is --bitbreite des werts wird erweitert min signExtension
     -- begin solution:
     generic (
         word_width : integer := WORD_WIDTH
     );
-    port (
+    port ( --immedate um direkt in der Instruktion erhalten 
         pi_instr : in std_logic_vector(word_width - 1 downto 0);
         po_jumpImm : out std_logic_vector(word_width - 1 downto 0);
         po_branchImm : out std_logic_vector(word_width - 1 downto 0);
-        po_unsignedImm : out std_logic_vector(word_width - 1 downto 0);
+        po_unsignedImm : out std_logic_vector(word_width - 1 downto 0);--für adressberechnung
         po_immediateImm : out std_logic_vector(word_width - 1 downto 0);
         po_storeImm : out std_logic_vector(word_width - 1 downto 0)
     );
@@ -41,26 +41,35 @@ architecture arc of signExtension is
 
 begin
     process (pi_instr)
-
-        variable v_immediateImm : signed (11 downto 0) := (others => '0');
-        variable v_storeImm : signed (11 downto 0) := (others => '0');
-        variable v_branchImm : signed (12 downto 0) := (others => '0');
-        variable v_jumpImm : signed (20 downto 0) := (others => '0');
+        variable v_immediateImm : signed (11 downto 0) := (others => '0'); --ALU-Operationen
+        variable v_storeImm : signed (11 downto 0) := (others => '0'); --Speicheroperationen
+        variable v_branchImm : signed (12 downto 0) := (others => '0'); --Sprung- und Verzweigungsoperationen
+        variable v_jumpImm : signed (20 downto 0) := (others => '0'); --Sprungoperationen
 
     begin
-        v_immediateImm := signed(pi_instr(31 downto 20));
-        po_immediateImm <= std_logic_vector(resize(v_immediateImm, word_width));
-
+        v_immediateImm := signed(pi_instr(31 downto 20)); --12 bit extrahierung, interpretation als signed 
+        po_immediateImm <= std_logic_vector(resize(v_immediateImm, word_width)); --Wert wird auf word_width erweitert
+        --signed und resize , um wert auf korrekte breite zu bringen
         v_storeImm := signed(pi_instr(31 downto 25) & pi_instr(11 downto 7));
         po_storeImm <= std_logic_vector(resize(v_storeImm, word_width));
-
+        --signed für vorzeichen behaftet
         v_branchImm := signed(pi_instr(31) & pi_instr(7) & pi_instr(30 downto 25) & pi_instr(11 downto 8) & '0');
         po_branchImm <= std_logic_vector(resize(v_branchImm, word_width));
-
+        --unsigned für vorzeichenlos
         v_jumpImm := signed(pi_instr(31) & pi_instr(19 downto 12) & pi_instr(20) & pi_instr(30 downto 21) & '0');
         po_jumpImm <= std_logic_vector(resize(v_jumpImm, word_width));
 
-        po_unsignedImm <= pi_instr(word_width - 1 downto 12) & (11 downto 0 => '0');
+        po_unsignedImm <= pi_instr(word_width - 1 downto 12) & (11 downto 0 => '0'); --extrahierung der oberen bits
     end process;
     -- end solution!!
 end architecture arc;
+
+--Wieso immedate-werte? Um direkt in der Instruktion erhalten um nicht extra in register zu speichern
+--Arithmetische Operationen auf den Werten
+--Sprung- und Verzweigung
+--Speicheroperationen um die adresse für den Speicherzugiff zu brechnen 
+
+--Wieso ist die erweiterung der Breite notwendig?
+--stellt sicher das auch kleinere werte in 32-bit register gespeichert werden können
+--bei negativen adresse stellt sicher das diese in einem größeren Adressraum Interpretiert werden
+
