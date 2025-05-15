fa8b1414712b86604bcc95806fdb15de33d15661
Maximilian Wolf <maxi645@outlook.de>
2025-05-15 11:16:49 +0200

    Added registerOut to registerFile

--- components/registerfile/register_file.vhdl
+++ components/registerfile/register_file.vhdl
@@ -1,7 +1,7 @@
 -- Laboratory RA solutions/versuch2
 -- Sommersemester 25
 -- Group Details
--- Lab Date:06.05.25
+-- Lab Date: 06.05.25
 -- 1. Participant First and  Last Name: Maximilian Wolf
 -- 2. Participant First and Last Name: Esad-Muhammed Cekmeci
 
@@ -15,8 +15,7 @@ library ieee;
 entity register_file is
     generic(
         word_width  : integer := WORD_WIDTH;
-        adr_width   : integer := REG_ADR_WIDTH;
-        reg_amount  : integer := 2**REG_ADR_WIDTH
+        adr_width   : integer := REG_ADR_WIDTH
     );
     port(
         pi_clk, pi_rst  : in STD_LOGIC;
@@ -26,13 +25,13 @@ entity register_file is
         pi_writeRegData : in std_logic_vector(word_width-1 downto 0);
         pi_writeEnable  : in std_logic;
         po_readRegData1 : out std_logic_vector(word_width-1 downto 0);
-        po_readRegData2 : out std_logic_vector(word_width-1 downto 0)
+        po_readRegData2 : out std_logic_vector(word_width-1 downto 0);
+        po_registerOut  : out registermemory
     );  
 end entity register_file;
 
 architecture behavior of register_file is
-type reg_array is array (0 to reg_amount-1) of std_logic_vector(word_width-1 downto 0); --konnte es mit registermemory aus type_package nicht zum laufen bringenb
-signal s_array : reg_array := (others => (others => '0')); --0te register wird beim start auf 0 gesetzt
+signal s_array : registermemory := (others => (others => '0')); --0te register wird beim start auf 0 gesetzt
 
 begin
     process(pi_clk)
@@ -44,10 +43,10 @@ begin
         elsif rising_edge(pi_clk) then --Lesen und Schreiben bei steigender Flanke
             po_readRegData1 <= s_array(to_integer(unsigned(pi_readRegAddr1)));
             po_readRegData2 <= s_array(to_integer(unsigned(pi_readRegAddr2)));
-
-            if pi_writeEnable = '1' and to_integer(unsigned(pi_writeRegAddr)) /= 0 then --Schreibnur wenn pi_writeEnable = 1 und pi_writeRegAddr != 0
+            if pi_writeEnable = '1' and to_integer(unsigned(pi_writeRegAddr)) /= 0 then --Schreib nur wenn pi_writeEnable = 1 und pi_writeRegAddr != 0
                 s_array(to_integer(unsigned(pi_writeRegAddr))) <= pi_writeRegData;
             end if;
         end if;
+        po_registerOut <= s_array;
     end process;
 end architecture behavior;
