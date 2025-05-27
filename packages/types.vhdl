7fada70b8febfe574739245f3c1a9b518e8b8712
Maximilian Wolf <maxi645@outlook.de>
2025-05-27 14:52:16 +0000

    Added new files for 6. Lab

--- packages/types.vhdl
+++ packages/types.vhdl
@@ -1,3 +1,10 @@
+-- Laboratory RA solutions/versuch6
+-- Sommersemester 25
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
 -- ========================================================================
 -- Author:       Marcel Rieß
 -- Last updated: 02.05.2025
@@ -14,6 +21,9 @@ package types is
     ALU_OP       : std_logic_vector(ALU_OPCODE_WIDTH - 1 downto 0); -- determines the ALU's operation
     I_IMM_SEL    : std_logic;                                       -- used as a MUX selector for i-Format Immediates
     REG_WRITE    : std_logic;
+    A_SEL        : std_logic; -- used as a MUX selector for ALU
+    PC_SEL        : std_logic; -- used as a MUX selector for PC
+    WB_SEL       :std_logic_vector( 1 downto 0);
   end record controlWord;
 
   -- allows initialization of control words, used in decoder
@@ -21,7 +31,10 @@ package types is
   (
   ALU_OP => (others => '0'),
   I_IMM_SEL  => '0',
-  REG_WRITE   => '0'
+  REG_WRITE   => '0',
+  A_SEL   => '0',
+  PC_SEL   => '0',
+  WB_SEL   => "00"
   );
 
   -- enum containig all instruction formats, used in decoder
