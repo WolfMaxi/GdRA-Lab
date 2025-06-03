e6ddfdd1335880301a01c8f1f3951143d6ab72a2
Maximilian Wolf <maxi645@outlook.de>
2025-06-03 15:34:56 +0000

    Removed scripts

--- scripts/riscv/riu_only_risc_tb.sh
+++ scripts/riscv/riu_only_risc_tb.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
+workdir="../../"
+rm "${workdir}vhdl/"*.cf
 
-ghdl -a --std=08 ../../packages/constant_package.vhdl
-ghdl -a --std=08 ../../packages/type_packages.vhdl
-ghdl -a --std=08 ../../packages/types.vhdl
-ghdl -a --std=08 ../../packages/Util_Asm_Package.vhdl
-ghdl -a --std=08 ../../components/alu/my_half_adder.vhdl
-ghdl -a --std=08 ../../components/alu/my_full_adder.vhdl
-ghdl -a --std=08 ../../components/alu/my_gen_n_bit_full_adder.vhdl
-ghdl -a --std=08 ../../components/register/PipelineRegister.vhdl
-ghdl -a --std=08 ../../components/cache/instruction_cache.vhdl
-ghdl -a --std=08 ../../components/register/controlwordregister.vhdl
-ghdl -a --std=08 ../../components/decoder/decoder.vhdl
-ghdl -a --std=08 ../../components/registerfile/register_file.vhdl
-ghdl -a --std=08 ../../components/multiplexer/gen_mux.vhdl
-ghdl -a --std=08 ../../components/multiplexer/gen_mux2to1.vhdl
-ghdl -a --std=08 ../../components/signExtender/signExtension.vhdl
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/constant_package.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/type_packages.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/types.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}packages/Util_Asm_Package.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_half_adder.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_full_adder.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_gen_n_bit_full_adder.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/register/PipelineRegister.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/cache/instruction_cache.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/register/controlwordregister.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/decoder/decoder.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/registerfile/register_file.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/multiplexer/gen_mux.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/multiplexer/gen_mux2to1.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/signExtender/signExtension.vhdl"
 # =============== ALU ===============
-ghdl -a --std=08 ../../components/alu/my_gen_xor.vhdl
-ghdl -a --std=08 ../../components/alu/my_gen_or.vhdl
-ghdl -a --std=08 ../../components/alu/my_gen_and.vhdl
-ghdl -a --std=08 ../../components/alu/my_shifter.vhdl
-ghdl -a --std=08 ../../components/alu/my_comparator.vhdl
-ghdl -a --std=08 ../../components/alu/my_alu.vhdl
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_gen_xor.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_gen_or.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_gen_and.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_shifter.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_comparator.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}components/alu/my_alu.vhdl"
 # ===================================
-ghdl -a --std=08 ../../riscv/RIU_only_RISC_V.vhdl
-ghdl -a --std=08 ../../testbenches/riscv/RIU_only_RISC_V_tb.vhdl
-ghdl -e --std=08 RIU_only_RISC_V_tb
-ghdl -r --std=08 RIU_only_RISC_V_tb --wave=riu-only_riscv.ghw --vcd=riu-only_riscv.vcd
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}riscv/RIU_only_RISC_V.vhdl"
+ghdl -a --std=08 --workdir="${workdir}vhdl" "${workdir}testbenches/riscv/RIU_only_RISC_V_tb.vhdl"
+ghdl -e --std=08 --workdir="${workdir}vhdl" RIU_only_RISC_V_tb
+ghdl -r --std=08 --workdir="${workdir}vhdl" RIU_only_RISC_V_tb --wave="${workdir}vhdl/vcd/riu-only_riscv.ghw" --vcd="${workdir}vhdl/vcd/riu-only_riscv.vcd"
